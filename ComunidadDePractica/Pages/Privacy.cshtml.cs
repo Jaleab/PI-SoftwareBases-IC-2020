deca8fe86abc06344af998a442a7c5c4b218390f
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-04-22 14:58:07 -0600

    Sirve visualizacion

--- ComunidadDePractica/Pages/Privacy.cshtml.cs
+++ ComunidadDePractica/Pages/Privacy.cshtml.cs
@@ -5,13 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ComunidadPractica.Pages
+namespace ComunidadDePractica.Pages
 {
-    public class IndexModel : PageModel
+    public class PrivacyModel : PageModel
     {
         public void OnGet()
         {
-
         }
     }
 }
