7eb59f1d94543a20e71ec93a1f09388d5b7c1b65
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-05-01 00:17:21 -0600

    Edwin y Javier. Modelo, Controlador, Procedimiento, Tabla y un servicio para Articulo

---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/ArticuloController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComunidadDePracticaMVC.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        // GET: Articulo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Articulo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Articulo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Articulo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Articulo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Articulo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Articulo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
