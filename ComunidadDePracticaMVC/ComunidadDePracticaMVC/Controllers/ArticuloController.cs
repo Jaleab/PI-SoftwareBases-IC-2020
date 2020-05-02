7f0ec8fce116265294092453804293d426e709fb
Lucia Elizondo <lesan2807@gmail.com>
2020-05-02 08:48:02 -0600

    Agregar modelo y vista de artículo de lo de Edwin y Javier

---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/ArticuloController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ComunidadDePracticaMVC.Models;
+
+namespace ComunidadDePracticaMVC.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        // GET: Articulo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Articulo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Articulo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulo/Create
+        [HttpPost]
+        public ActionResult Create(ArticuloModel articulo)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Articulo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Articulo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, ArticuloModel articulo)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Articulo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Articulo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, ArticuloModel articulo)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
