fe63ad27fa9a8cf2331e9325b1c5dc9d2de97ed9
Lucia Elizondo <lesan2807@gmail.com>
2020-05-26 19:15:20 -0600

    Agregar MVC para File Falta que se conecte a la BD del proyecto

---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Services/FileService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using ComunidadDePracticaMVC.Models;
+
+using System.IO;
+using System.Web.Mvc;
+
+namespace ComunidadDePracticaMVC.Services
+{
+    public class FileService
+    {
+        private SqlConnection con;
+        public class FileInfo
+        {
+            public byte[] bytes;
+            public string contentType, filename; 
+            
+            public FileInfo(byte[] bytes, string contentType, string fileName)
+            {
+                this.bytes = bytes;
+                this.contentType = contentType;
+                this.filename = fileName;
+            }
+        }
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["Grupo3Conn"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        // Obtener archivo
+        public List<FileModel> GetFiles()
+        {
+            List<FileModel> files = new List<FileModel>();
+            string constr = ConfigurationManager.ConnectionStrings["LocalConn"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT Id, Name FROM tblFiles"))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            files.Add(new FileModel
+                            {
+                                Id = Convert.ToInt32(sdr["Id"]),
+                                Name = sdr["Name"].ToString()
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            return files;
+        }
+
+        // Subir archivo
+        public void SubirArchivo(HttpPostedFileBase postedFile)
+        {
+            byte[] bytes;
+            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
+            {
+                bytes = br.ReadBytes(postedFile.ContentLength);
+            }
+            string constr = ConfigurationManager.ConnectionStrings["LocalConn"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "INSERT INTO tblFiles VALUES (@Name, @ContentType, @Data)";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@Name", Path.GetFileName(postedFile.FileName));
+                    cmd.Parameters.AddWithValue("@ContentType", postedFile.ContentType);
+                    cmd.Parameters.AddWithValue("@Data", bytes);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+
+
+        // Descargar archivo
+        public FileInfo DownloadFile(int? fileId)
+        {
+            byte[] bytes;
+            string fileName, contentType;
+            string constr = ConfigurationManager.ConnectionStrings["LocalConn"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "SELECT Name, Data, ContentType FROM tblFiles WHERE Id=@Id";
+                    cmd.Parameters.AddWithValue("@Id", fileId);
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        bytes = (byte[])sdr["Data"];
+                        contentType = sdr["ContentType"].ToString();
+                        fileName = sdr["Name"].ToString();
+                    }
+                    con.Close();
+                }
+            }
+
+            return new FileInfo(bytes, contentType, fileName);
+        }
+
+    }
+}
