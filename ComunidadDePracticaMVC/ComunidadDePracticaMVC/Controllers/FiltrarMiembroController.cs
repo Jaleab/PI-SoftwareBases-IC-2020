863fc9fff964ea9ac80dd04e9f59630892ac14ed
Lucia Elizondo <lesan2807@gmail.com>
2020-06-24 16:35:24 -0600

    Agregar vista para el filtro de miembros

---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/FiltrarMiembroController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ComunidadDePracticaMVC.Models;
+
+namespace ComunidadDePracticaMVC.Controllers
+{
+    public class FiltrarMiembroController : Controller
+    {
+        // GET: FiltrarMiembro
+        public ActionResult FiltrarMiembro(FiltrarMiembroModel filtro)
+        {
+            filtro.Paises = new SelectList("Costa Rica", "Panama");
+            filtro.Idiomas = new SelectList("Costa Rica", "Panama");
+            filtro.Habilidades = new SelectList("Costa Rica", "Panama");
+            filtro.Hobbies = new SelectList("Costa Rica", "Panama");
+            return View(filtro);
+        }
+    }
+}
---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Models/FiltrarMiembroModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComunidadDePracticaMVC.Models
+{
+    public class FiltrarMiembroModel
+    {
+        public string paisSeleccionado { get; set; }
+
+        public IEnumerable<SelectListItem> Paises { get; set; }
+
+        public string habilidadSeleccionado { get; set; }
+
+        public IEnumerable<SelectListItem> Habilidades { get; set; }
+
+        public string idiomaSeleccionado { get; set; }
+
+        public IEnumerable<SelectListItem> Idiomas { get; set; }
+        public string hobbieSeleccionado { get; set; }
+
+        public IEnumerable<SelectListItem> Hobbies { get; set; }
+
+
+    }
+}
