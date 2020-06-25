ef290a37fa6254f65d25693c7977482cd9e63d57
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-06-25 01:14:55 -0600

    Vistas y consultas para articulos quieren revisores y decidir revisores. Faltan funciones botones

--- ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/RevisionesController.cs
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/RevisionesController.cs
@@ -101,9 +101,7 @@ namespace ComunidadDePracticaMVC.Controllers
 
         [Authorize]
         public ActionResult AceptarColaboracion(int articuloId)
-        {
-
-        
+        {        
             if (AuthorizeClass.AuthorizeRole(Request.Cookies[FormsAuthentication.FormsCookieName], "Núcleo,Coordinador"))
             {
                 RevisionesService servicioRevisiones = new RevisionesService();
@@ -148,6 +146,45 @@ namespace ComunidadDePracticaMVC.Controllers
             {
                 return new RedirectResult("~/AccessDenied");
             }
+        }
+
+        public ActionResult ArticulosRequierenRevisores()
+        {
+            if (TempData["Message"] != null)
+            {
+                ViewBag.Message = TempData["Message"].ToString();
+            }
+            ViewBag.categoria = "";
+            ViewBag.revisiones = new RevisionesModel();
+            RevisionesService servicioRevisiones = new RevisionesService();
+            if (User.Identity.IsAuthenticated)
+            {
+                string correo = User.Identity.Name.ToString();
+                UsuarioService servicioUsuarios = new UsuarioService();
+                var datos = servicioUsuarios.GetDatosMiembro(correo);
+                ViewBag.categoria = datos[0];
+                ViewBag.revisiones = servicioRevisiones.ObtenerArticulosRequierenRevisores();
+            }
+            return View();
+        }
+
+        public ActionResult DecidirRevisores(int articuloId)
+        {
+            if (TempData["Message"] != null)
+            {
+                ViewBag.Message = TempData["Message"].ToString();
+            }
+            ViewBag.categoria = "";
+            ViewBag.revisiones = new RevisionesModel();
+            RevisionesService servicioRevisiones = new RevisionesService();
+            if (User.Identity.IsAuthenticated)
+            {
+                string correo = User.Identity.Name.ToString();
+                UsuarioService servicioUsuarios = new UsuarioService();
+                var datos = servicioUsuarios.GetDatosMiembro(correo);
+                ViewBag.categoria = datos[0];
+            }
+            return View(servicioRevisiones.ObtenerPosiblesRevisores(articuloId));
         }
     }
 }
