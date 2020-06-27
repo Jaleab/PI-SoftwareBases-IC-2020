9b1d8fb7cd72135c7e72798ae284d41e1b198436
Lucia Elizondo <lesan2807@gmail.com>
2020-06-26 23:00:34 -0600

    graficos para los datos

--- ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/ChartDataController.cs
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Controllers/ChartDataController.cs
@@ -15,10 +15,28 @@ namespace ComunidadDePracticaMVC.Controllers
         {
             ChartService chartData = new ChartService();
             var labelPais = chartData.ObtenerPaises();
-            var numbers = chartData.ObtenerCuentaPaises();
+            var numbersPais = chartData.ObtenerCuentaPaises();
             
             ViewBag.LabelPais = Newtonsoft.Json.JsonConvert.SerializeObject(labelPais);
-            ViewBag.DataPais = Newtonsoft.Json.JsonConvert.SerializeObject(numbers);
+            ViewBag.DataPais = Newtonsoft.Json.JsonConvert.SerializeObject(numbersPais);
+
+            var labelHabilidad = chartData.ObtenerHabilidades();
+            var numbersHabilidad = chartData.ObtenerCuentaHabilidades();
+
+            ViewBag.LabelHabilidad = Newtonsoft.Json.JsonConvert.SerializeObject(labelHabilidad);
+            ViewBag.DataHabilidad = Newtonsoft.Json.JsonConvert.SerializeObject(numbersHabilidad);
+
+            var labelHobbie = chartData.ObtenerHobbies();
+            var numbersHobbie = chartData.ObtenerCuentaHobbies();
+
+            ViewBag.LabelHobbie = Newtonsoft.Json.JsonConvert.SerializeObject(labelHobbie);
+            ViewBag.DataHobbie = Newtonsoft.Json.JsonConvert.SerializeObject(numbersHobbie);
+
+            var labelIdioma = chartData.ObtenerIdiomas();
+            var numbersIdioma = chartData.ObtenerCuentaIdiomas();
+
+            ViewBag.LabelIdioma = Newtonsoft.Json.JsonConvert.SerializeObject(labelIdioma);
+            ViewBag.DataIdioma = Newtonsoft.Json.JsonConvert.SerializeObject(numbersIdioma);
 
 
             return View(data);
--- ComunidadDePracticaMVC/ComunidadDePracticaMVC/Services/ChartService.cs
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC/Services/ChartService.cs
@@ -43,6 +43,91 @@ namespace ComunidadDePracticaMVC.Services
             return listaPaises;
         }
 
+        public List<string> ObtenerHabilidades()
+        {
+            List<string> listaPaises = new List<string>();
+            connection();
+            string consulta =
+               "SELECT DISTINCT habilidadUsuario " +
+               "FROM Habilidad, Usuario " +
+               "WHERE Habilidad.correoUsuarioFK = Usuario.correo " +
+               "AND habilidadUsuario = NULLIF(habilidadUsuario,'')";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                listaPaises.Add(Convert.ToString(dr["habilidadUsuario"]));
+            }
+
+            return listaPaises;
+        }
+
+        public List<string> ObtenerHobbies()
+        {
+            List<string> listaHobbies = new List<string>();
+            connection();
+            string consulta =
+               "SELECT DISTINCT hobbieUsuario " +
+               "FROM Hobbie, Usuario " +
+               "WHERE Hobbie.correoUsuarioFK = Usuario.correo " +
+               "AND hobbieUsuario = NULLIF(hobbieUsuario,'')";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                listaHobbies.Add(Convert.ToString(dr["hobbieUsuario"]));
+            }
+
+            return listaHobbies;
+        }
+
+        public List<string> ObtenerIdiomas()
+        {
+            List<string> listaIdiomas = new List<string>();
+            connection();
+            string consulta =
+               "SELECT DISTINCT idiomaUsuario " +
+               "FROM Idioma, Usuario " +
+               "WHERE Idioma.correoUsuarioFK = Usuario.correo " +
+               "AND idiomaUsuario = NULLIF(idiomaUsuario,'')";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                listaIdiomas.Add(Convert.ToString(dr["idiomaUsuario"]));
+            }
+
+            return listaIdiomas;
+        }
+
+
         public List<int> ObtenerCuentaPaises()
         {
             List<String> paises = this.ObtenerPaises(); 
@@ -76,5 +161,111 @@ namespace ComunidadDePracticaMVC.Services
             }
             return listaValores;
         }
+
+        public List<int> ObtenerCuentaHabilidades()
+        {
+            List<String> habilidades = this.ObtenerHabilidades(); 
+            List<int> listaValores = new List<int>();
+            connection();
+
+
+            string consulta =
+               "SELECT COUNT(habilidadUsuario) AS 'VALOR' " +
+               "FROM Habilidad, Usuario " +
+               "WHERE Habilidad.correoUsuarioFK = Usuario.correo " +
+               "AND habilidadUsuario = @habilidadParam";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            for (int i = 0; i < habilidades.Count(); ++i)
+            {
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@habilidadParam", habilidades[i]);
+                dt.Clear();
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                con.Open();
+                sd.Fill(dt);
+                con.Close();
+
+                DataRow dr = dt.Rows[0];
+                listaValores.Add(Convert.ToInt32(dr["VALOR"]));
+            }
+            return listaValores;
+        }
+
+        public List<int> ObtenerCuentaHobbies()
+        {
+            List<String> hobbies = this.ObtenerHobbies();
+            List<int> listaValores = new List<int>();
+            connection();
+
+
+            string consulta =
+               "SELECT COUNT(hobbieUsuario) AS 'VALOR' " +
+               "FROM Hobbie, Usuario " +
+               "WHERE Hobbie.correoUsuarioFK = Usuario.correo " +
+               "AND hobbieUsuario = @hobbieParam";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            for (int i = 0; i < hobbies.Count(); ++i)
+            {
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@hobbieParam", hobbies[i]);
+                dt.Clear();
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                con.Open();
+                sd.Fill(dt);
+                con.Close();
+
+                DataRow dr = dt.Rows[0];
+                listaValores.Add(Convert.ToInt32(dr["VALOR"]));
+            }
+            return listaValores;
+        }
+
+        public List<int> ObtenerCuentaIdiomas()
+        {
+            List<String> idomas = this.ObtenerIdiomas();
+            List<int> listaValores = new List<int>();
+            connection();
+
+
+            string consulta =
+               "SELECT COUNT(idiomaUsuario) AS 'VALOR' " +
+               "FROM Idioma, Usuario " +
+               "WHERE Idioma.correoUsuarioFK = Usuario.correo " +
+               "AND idiomaUsuario = @idiomaUsuario";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            for (int i = 0; i < idomas.Count(); ++i)
+            {
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@idiomaUsuario", idomas[i]);
+                dt.Clear();
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                con.Open();
+                sd.Fill(dt);
+                con.Close();
+
+                DataRow dr = dt.Rows[0];
+                listaValores.Add(Convert.ToInt32(dr["VALOR"]));
+            }
+            return listaValores;
+        }
     }
 }
+
