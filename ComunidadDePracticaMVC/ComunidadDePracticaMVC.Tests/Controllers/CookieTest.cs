2b135cd8c33a9449cf7e8a516784b7016a38c8b4
Felipe <felipesanchezbaldi@gmail.com>
2020-06-30 07:02:15 -0600

    Test de verificar login

--- ComunidadDePracticaMVC/ComunidadDePracticaMVC.Tests/Controllers/CookieTest.cs
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC.Tests/Controllers/CookieTest.cs
@@ -13,7 +13,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         [TestMethod]
         public void CookieTest_UserCoordinador_ReturnTrue() {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Coordinador");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Coordinador");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Coordinador");
             Assert.IsTrue(resultado);
         }
 
@@ -21,7 +21,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTest_UserCoordinador_ReturnFalse()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Coordinador");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Nucleo");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Nucleo");
             Assert.IsFalse(resultado);
         }
 
@@ -29,7 +29,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTestVariousUsers_UserCoordinador_ReturnTrue()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Coordinador");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Coordinador,Nucleo,Prueba,Prueba2");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Coordinador,Nucleo,Prueba,Prueba2");
             Assert.IsTrue(resultado);
         }
 
@@ -37,7 +37,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTestVariousUsers_UserCoordinador_ReturnFalse()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Coordinador");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Nucleo,Prueba,Prueba2,Periferico");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Nucleo,Prueba,Prueba2,Periferico");
             Assert.IsFalse(resultado);
         }
 
@@ -46,7 +46,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTest_UserNucleo_ReturnTrue()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Nucleo");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Nucleo");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Nucleo");
             Assert.IsTrue(resultado);
         }
 
@@ -54,7 +54,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTest_UserNucleo_ReturnFalse()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Nucleo");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Coordinador");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Coordinador");
             Assert.IsFalse(resultado);
         }
 
@@ -62,7 +62,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTestVariousUsers_UserNucleo_ReturnTrue()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Nucleo");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Nucleo,Coordinador,Periferico,Prueba2");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Nucleo,Coordinador,Periferico,Prueba2");
             Assert.IsTrue(resultado);
         }
 
@@ -70,7 +70,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTestVariousUsers_UserNucleo_ReturnFalse()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Nucleo");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Coordinador,Prueba,Prueba2,Periferico");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Coordinador,Prueba,Prueba2,Periferico");
             Assert.IsFalse(resultado);
         }
 
@@ -79,7 +79,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTest_UserPeriferico_ReturnTrue()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Periferico");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Periferico");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Periferico");
             Assert.IsTrue(resultado);
         }
 
@@ -87,7 +87,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTest_UserPeriferico_ReturnFalse()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Periferico");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Nucleo");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Nucleo");
             Assert.IsFalse(resultado);
         }
 
@@ -95,7 +95,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTestVariousUsers_UserPeriferico_ReturnTrue()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Periferico");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Coordinador,Nucleo,Prueba,Periferico");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Coordinador,Nucleo,Prueba,Periferico");
             Assert.IsTrue(resultado);
         }
 
@@ -103,7 +103,7 @@ namespace ComunidadDePracticaMVC.Tests.Controllers
         public void CookieTestVariousUsers_UserPeriferico_ReturnFalse()
         {
             System.Web.HttpCookie auth = CookieTestController.CreateCookieDummy("Periferico");
-            bool resultado = Models.CookieHandler.AuthorizeRole(auth, "Nucleo,Prueba,Prueba2,Coordinador");
+            bool resultado = Services.CookieHandler.AuthorizeRole(auth, "Nucleo,Prueba,Prueba2,Coordinador");
             Assert.IsFalse(resultado);
         }
     }
---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC.Tests/Controllers/LoginTest.cs
@@ -0,0 +1,113 @@
+﻿using ComunidadDePracticaMVC.Controllers;
+using ComunidadDePracticaMVC.Models;
+using ComunidadDePracticaMVC.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComunidadDePracticaMVC.Tests.Controllers
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void VerificarLogin_DatosCorrectos() {
+            //Arrange
+            AccountService service = new AccountService();
+            LoginViewModel model = new LoginViewModel
+            {
+                Email = "edwin.brenes.c@gmail.com",
+                Password = "delfin"
+            };
+            //Act
+            int resultado = service.LoginUser (model);
+
+            //Assert 
+            Assert.AreEqual(1, resultado);
+        }
+        [TestMethod]
+        public void VerificarLogin_DatosCorrectosAllCaps() {
+
+            //Arrange
+            AccountService service = new AccountService();
+            LoginViewModel model = new LoginViewModel
+            {
+                Email = "EDWIN.BRENES.C@GMAIL.COM",
+                Password = "DELFIN"
+            };
+            //Act
+            int resultado = service.LoginUser(model);
+
+            //Assert 
+            Assert.AreEqual(-1, resultado);
+
+        }
+        [TestMethod]
+        public void VerificarLogin_DatosCorrectosMinuscula() {
+            //Arrange
+            AccountService service = new AccountService();
+            LoginViewModel model = new LoginViewModel
+            {
+                Email = "allcapsDELFIN@gmail.com",
+                Password = "delfin"
+            };
+            //Act
+            int resultado = service.LoginUser(model);
+
+            //Assert 
+            Assert.AreEqual(-1, resultado);
+
+        }
+        [TestMethod]
+        public void VerificarLogin_DatosIncorrectos() {
+            //Arrange
+            AccountService service = new AccountService();
+            LoginViewModel model = new LoginViewModel
+            {
+                Email = "aasdasd@cmas.com",
+                Password = "de1212n"
+            };
+            //Act
+            int resultado = service.LoginUser(model);
+
+            //Assert 
+            Assert.AreEqual(-1, resultado);
+        }
+        [TestMethod]
+        public void VerificarLogin_NombreIncorrecto() {
+            //Arrange
+            AccountService service = new AccountService();
+            LoginViewModel model = new LoginViewModel
+            {
+                Email = "aasdasd@cmas.com",
+                Password = "delfin"
+            };
+            //Act
+            int resultado = service.LoginUser(model);
+
+            //Assert 
+            Assert.AreEqual(-1, resultado);
+
+        }
+        [TestMethod]
+        public void VerificarLogin_ContraseñaIncorrecta() {
+            //Arrange
+            AccountService service = new AccountService();
+            LoginViewModel model = new LoginViewModel
+            {
+                Email = "edwin.brenes.c@gmail.com",
+                Password = "asdasd"
+            };
+            //Act
+            int resultado = service.LoginUser(model);
+
+            //Assert 
+            Assert.AreEqual(-1, resultado);
+        }
+
+
+    }
+}
