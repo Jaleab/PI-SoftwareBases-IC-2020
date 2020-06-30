65dbd1b3d132d2b96596d0ef64a59d83cd6dfca3
Lucia Elizondo <lesan2807@gmail.com>
2020-06-30 02:19:43 -0600

    Test para vista de InicioArticulos

---
+++ ComunidadDePracticaMVC/ComunidadDePracticaMVC.Tests/Controllers/ArticuloTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PassParameter.Controllers;
+using ComunidadDePracticaMVC.Controllers;
+using System.Web.Mvc;
+using ComunidadDePracticaMVC.Models;
+using ComunidadDePracticaMVC.Services;
+
+namespace ComunidadDePracticaMVC.Tests.Controllers
+{
+    [TestClass]
+    public class ArticuloTest
+    {
+        [TestMethod]
+        public void TestInicioArticulosNotNull()
+        {
+            ArticuloController artController = new ArticuloController();
+
+            ViewResult result = artController.InicioArticulos() as ViewResult;
+
+            Assert.IsNotNull(result); 
+        }
+
+        [TestMethod]
+        public void TestCrearArticulo()
+        {
+            ArticuloService artService = new ArticuloService();
+
+            ArticuloController artController = new ArticuloController();
+
+            ViewResult result = artController.Create() as ViewResult;
+        }
+
+
+    }
+}
